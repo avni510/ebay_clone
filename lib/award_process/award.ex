a19a6eefe3226c9060c76c83bdea4db45eedf09d
Avni Kothari <avnikothari@Avnis-MacBook-Air.local>
2017-06-29 07:18:24 -0500

    Assigned winner_id in Award Process
    * wrote a query to find the winner_id and assign it to an item
    that is awarded
    * placed it in the award process

--- lib/award_process/award.ex
+++ lib/award_process/award.ex
@@ -3,15 +3,16 @@ defmodule EbayClone.AwardProcess.Award do
 
   alias EbayClone.Repo
   alias EbayClone.Item
+  alias EbayClone.Bid
 
   def execute do
-    items_to_be_updated = Repo.all(query())
+    items_to_be_updated = Repo.all(items_to_be_awarded_query())
     Enum.map(items_to_be_updated, fn(item) -> update_item(item) end)
     twenty_four_hours = 24 * 60 * 60 * 1000
     Process.send_after(self(), :award, twenty_four_hours)
   end
 
-  defp query do
+  defp items_to_be_awarded_query do
     date = DateTime.utc_now()
     from item in Item,
       where: item.end_date <= ^date and
@@ -19,7 +20,20 @@ defmodule EbayClone.AwardProcess.Award do
   end
 
   defp update_item(item) do
-    changeset = Item.changeset(item, %{awarded: true})
+    winner_id = get_winner_id(item)
+    changeset = Item.changeset(item, %{awarded: true, winner_id: winner_id})
     Repo.update(changeset)
+  end
+
+  defp find_highest_bid_query(item) do
+    from bid in Bid,
+      where: ^item.id == bid.item_id,
+      order_by: [desc: bid.price],
+      limit: 1
+  end
+
+  defp get_winner_id(item) do
+    bid = Repo.one(find_highest_bid_query(item))
+    bid.user_id
   end
 end
--- test/lib/award_process/award_test.exs
+++ test/lib/award_process/award_test.exs
@@ -1,40 +1,50 @@
 defmodule EbayClone.AwardProcess.AwardTest do
   use EbayClone.ConnCase
   import EbayClone.ItemCase
+  import EbayClone.BidCase
+  import EbayClone.UserCase
 
   alias EbayClone.AwardProcess.Award
   alias EbayClone.Item
   alias EbayClone.Repo
 
   describe "execute" do
-    test "it changes the item awarded attribute to true for entries with an end date in the past the awarded set to false" do
+    test "it changes the item awarded attribute to true and updates the winner_id attribute for entries with an end date in the past the awarded set to false" do
       {:ok, item_1} = create_item("foo@example.com",
                                   "test password",
                                   %{DateTime.utc_now | second: DateTime.utc_now.second + 1},
                                   "item 1",
                                   "bar",
                                   42)
-      create_item("bar@example.com",
-                   "test password",
-                   %{DateTime.utc_now | year: DateTime.utc_now.year + 1},
-                   "item 2",
-                   "test description",
-                   10)
+      {:ok, user_1} = create_user("testuser@example.com", "test password")
+      {:ok, user_2} = create_user("fakeuser@example.com", "fizzbuzz")
+      create_bid(50, item_1.id, user_1.id)
+      create_bid(60, item_1.id, user_2.id)
+      create_bid(90, item_1.id, user_1.id)
+      {:ok, item_2} = create_item("bar@example.com",
+                                  "test password",
+                                  %{DateTime.utc_now | year: DateTime.utc_now.year + 1},
+                                  "item 2",
+                                  "test description",
+                                  10)
+      create_bid(20, item_2.id, user_1.id)
+      create_bid(30, item_2.id, user_2.id)
       :timer.sleep(1000)
 
       Award.execute()
 
       updated_item = Repo.get(Item, item_1.id)
       assert updated_item.awarded == true
+      assert updated_item.winner_id == user_1.id
     end
 
-    test "it does not change the item awarded attribute to true for entries with an end date in the future" do
+    test "it does not change the item awarded attribute to true and does not update the winner_id attribute for entries with an end date in the future" do
       create_item("foo@example.com",
-                   "test password",
-                   %{DateTime.utc_now | second: DateTime.utc_now.year + 1},
-                   "item 1",
-                   "bar",
-                   42)
+                  "test password",
+                  %{DateTime.utc_now | second: DateTime.utc_now.year + 1},
+                  "item 1",
+                  "bar",
+                  42)
       {:ok, item_2} = create_item("bar@example.com",
                                   "test password",
                                   %{DateTime.utc_now | year: DateTime.utc_now.year + 1},
@@ -46,6 +56,7 @@ defmodule EbayClone.AwardProcess.AwardTest do
 
       item = Repo.get(Item, item_2.id)
       assert item.awarded == false
+      assert item.winner_id == nil
     end
   end
 end
